f106fd1b3cbb82f8ebbce293044effe7f38faca6
Julien Viet <julien@julienviet.com>
2022-10-12 09:42:47 +0200

    Javadoc

--- vertx-async-await-incubator/src/main/java/io/vertx/await/impl/Scheduler.java
+++ vertx-async-await-incubator/src/main/java/io/vertx/await/impl/Scheduler.java
@@ -3,10 +3,20 @@ package io.vertx.await.impl;
 import java.util.concurrent.Executor;
 import java.util.function.Consumer;
 
+/**
+ * Schedule context tasks for execution.
+ *
+ * Context tasks are serialized in a queue.
+ */
 public interface Scheduler extends Executor {
 
   boolean inThread();
 
+  /**
+   * Signals the scheduler the current thread does not own anymore the scheduler permit.
+   *
+   * @return an object to schedule a task for execution, the scheduled tasks preempts the scheduler task queue
+   */
   Consumer<Runnable> detach();
 
 }
